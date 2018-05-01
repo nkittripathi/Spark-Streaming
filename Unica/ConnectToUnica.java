77a1dd77a3f6505e69b07355cb03302372a11df1
nkittripathi <at186045@teradata.com>
2018-05-01 20:57:38 +0530

    Update ConnectToUnica.java

--- Unica/ConnectToUnica.java
+++ Unica/ConnectToUnica.java
@@ -35,6 +35,10 @@ import org.apache.nifi.processor.ProcessorInitializationContext;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.util.StandardValidators;
 
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -50,6 +54,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -112,7 +118,30 @@ public class ConnectToUnica extends AbstractProcessor {
 			.displayName("Proxy Host")
 			.description("Provide Client Refresh Token")
 			.required(false)
-			.defaultValue("10.10.2.88")
+			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+			.build();
+
+	public static final PropertyDescriptor EMAIL_ID = new PropertyDescriptor
+			.Builder().name("Email Id")
+			.displayName("Email Ids")
+			.description("Provide Email Id")
+			.required(false)
+			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+			.build();
+
+	public static final PropertyDescriptor CONTACT_NUMBER = new PropertyDescriptor
+			.Builder().name("Contact Number")
+			.displayName("Contact Number")
+			.description("Provide Contact Number")
+			.required(false)
+			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+			.build();
+
+	public static final PropertyDescriptor CONTENT = new PropertyDescriptor
+			.Builder().name("Content")
+			.displayName("Content")
+			.description("Content to be send")
+			.required(false)
 			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
 			.build();
 
@@ -121,7 +150,6 @@ public class ConnectToUnica extends AbstractProcessor {
 			.displayName("Proxy Port")
 			.description("Provide Proxy Port")
 			.required(false)
-			.defaultValue("8080")
 			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
 			.build();
 
@@ -147,6 +175,9 @@ public class ConnectToUnica extends AbstractProcessor {
 		descriptors.add(CLIENT_ID);
 		descriptors.add(CLIENT_SECRET_ID);
 		descriptors.add(CLIENT_REFRESH_TOKEN);
+		descriptors.add(EMAIL_ID);
+		descriptors.add(CONTACT_NUMBER);
+		descriptors.add(CONTENT);
 		descriptors.add(PROXY_HOST);
 		descriptors.add(PROXY_PORT);
 		this.descriptors = Collections.unmodifiableList(descriptors);
@@ -173,165 +204,254 @@ public class ConnectToUnica extends AbstractProcessor {
 	}
 
 	URL url;
-	String sendEncoding;
+	String sendEncoding = "utf-8";
 	String wcaAuthURL;
-	String emailURL;
-	String smsURL;
-	String clientID;
-	String clientSecretID;
-	String clientRefreshToken;
-	String response;
-	String proxyPort;
-	String proxyHost;
-	String token;
+	String accessToken;
 	Proxy proxy;
-	FlowFile flowFile;
 	boolean isProxyset;
 	boolean accessTokenSet = true;
+	boolean validToken = false;
 	ComponentLog logger;
+	final String SUCCESS = "<SUCCESS>(.+?)</SUCCESS>";
+	final String FAULT_CODE = "<FaultCode>(.+?)</FaultCode>";
+	final String FAULT_MESSAGE = "<FaultString>(.+?)</FaultString>";
+	final String SPACE ="";
 
 	@Override
 	public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
 		logger = getLogger();
-		logger.info("###### 1 in onTrigger");
-		flowFile = session.get();
+		FlowFile flowFile = session.get();
 
 		if (flowFile == null) {
 			return;
 		}
 
+		String response;
+		String emailURL = context.getProperty(EMAIL_URL).evaluateAttributeExpressions(flowFile).getValue().trim();
+		String smsURL = context.getProperty(SMS_URL).evaluateAttributeExpressions(flowFile).getValue().trim();
+		String clientID = context.getProperty(CLIENT_ID).evaluateAttributeExpressions(flowFile).getValue().trim();
+		String clientSecretID = context.getProperty(CLIENT_SECRET_ID).evaluateAttributeExpressions(flowFile).getValue().trim();
+		String clientRefreshToken = context.getProperty(CLIENT_REFRESH_TOKEN).evaluateAttributeExpressions(flowFile).getValue().trim();		
+		String proxyPort ="";
+		String proxyHost = "";
 		wcaAuthURL = context.getProperty(WCA_AUTH_URL).evaluateAttributeExpressions(flowFile).getValue().trim();
-		emailURL = context.getProperty(EMAIL_URL).evaluateAttributeExpressions(flowFile).getValue().trim();
-		smsURL = context.getProperty(SMS_URL).evaluateAttributeExpressions(flowFile).getValue().trim();
-		clientID = context.getProperty(CLIENT_ID).evaluateAttributeExpressions(flowFile).getValue().trim();
-		clientSecretID = context.getProperty(CLIENT_SECRET_ID).evaluateAttributeExpressions(flowFile).getValue().trim();
-		clientRefreshToken = context.getProperty(CLIENT_REFRESH_TOKEN).evaluateAttributeExpressions(flowFile).getValue().trim();
-		proxyHost = context.getProperty(PROXY_HOST).evaluateAttributeExpressions(flowFile).getValue().trim();
-		proxyPort = context.getProperty(PROXY_PORT).evaluateAttributeExpressions(flowFile).getValue().trim();
-		AccessTokenRetriever accessTokenRetriever = new AccessTokenRetriever(wcaAuthURL);
-		
-		HttpsURLConnection httpsURLConn = null;
-		HttpURLConnection httpURLConn = null;
-		String emailRequest = "<Envelope><Body><SendMailing>\r\n" +
-				"<MailingId>11</MailingId>\r\n" +
-				"<RecipientEmail>xyz@bank.com</RecipientEmail>\r\n" +
-				"</SendMailing>\r\n" +
-				"</Body></Envelope>";
 
+		if (context.getProperty(PROXY_HOST).isSet()) {
+			proxyPort = context.getProperty(PROXY_HOST).evaluateAttributeExpressions(flowFile).getValue().trim();			
+		}
+		if (context.getProperty(PROXY_PORT).isSet()) {
+			proxyHost = context.getProperty(PROXY_PORT).evaluateAttributeExpressions(flowFile).getValue().trim();		
+		}
 
-		String smsRequest = "{\n" + 
-				"  \"content\": \"Hey Test SMS from Silverpop\",\n" + 
-				"  \"contacts\": [\n" + 
-				"    {\n" + 
-				"      \"contactId\": \"11111\",\n" + 
-				"      \"contactLookup\": [\n" + 
-				"        {\n" + 
-				"          \"name\": \"Mobile_No\",\n" + 
-				"          \"value\": \"9876543210\",\n" + 
-				"          \"channel\": \"SMS\"\n" + 
-				"        }\n" + 
-				"      ]\n" + 
-				"    }\n" + 
-				"  ],\n" + 
-				"  \"channelQualifier\": \"140084\"\n" + 
-				"}";
+		String content = context.getProperty(CONTENT).evaluateAttributeExpressions(flowFile).getValue().trim();
+		String emailId = context.getProperty(CONTENT).evaluateAttributeExpressions(flowFile).getValue().trim();
+		String contactNumber = context.getProperty(CONTENT).evaluateAttributeExpressions(flowFile).getValue().trim();
 
+		String emailRequest = formRequestXMLBody(content);
 
-		sendEncoding = "utf-8";
+		String smsRequest = formRequestJSONBody(content);
+
+		logger.info(" clientID "+ clientID);
+		logger.info(" clientSecretID "+ clientSecretID);
+		logger.info(" clientRefreshToken "+ clientRefreshToken);
+		logger.info("Email send time : "+ System.currentTimeMillis());
 
 		try {
-			logger.info("Getting token");
-			if (proxyHost.isEmpty() || proxyPort.isEmpty()) {
-				token = accessTokenRetriever.retrieveToken(clientID ,clientSecretID, clientRefreshToken, proxyHost, Integer.parseInt(proxyPort));
-				proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, Integer.parseInt(proxyPort)));
-				isProxyset = true;
-			} else {				
-				isProxyset = false;
-				token = accessTokenRetriever.retrieveToken(clientID ,clientSecretID, clientRefreshToken);		
-			}
-			if (token==null) {
-				return;
-			}
-			httpsURLConn = sendEmail(new URL(emailURL));
-			response = getHttpsResponse(httpsURLConn, token, emailRequest);
-			
-			httpURLConn = sendSMS(new URL(smsURL));
-			response = getHttpResponse(httpURLConn, token, smsRequest);
-			System.out.println(response);
-			session.transfer(flowFile, success);
+			logger.info("Getting access token");
+			if (validToken) {	
+				logger.info("Already have valid access token");
 
+			} else {		
+				accessToken = getTokenResponse(clientID,clientSecretID, clientRefreshToken, proxyHost, proxyPort);
+				validToken = true;
+				logger.info("Token recieving time : "+ System.currentTimeMillis());
+				logger.info("Token Response is : " + accessToken);
+			}
+			response = sendEmail(new URL(emailURL), accessToken, emailRequest);
+			if (getSuccessResult(response).equals("true")) {
+
+				logger.info("response is : " + response);
+
+			} else if (getFailResult(response).equals("invalid_token")){
+
+				accessToken = getTokenResponse(clientID,clientSecretID, clientRefreshToken, proxyHost, proxyPort);	
+				response = sendEmail(new URL(emailURL), accessToken, emailRequest);
+			}
+
+			logger.info("Email Delivered Time : "+ System.currentTimeMillis());
+			logger.info("response is : " + response);
+
+			logger.info("Sending SMS");
+			response = null;
+			response = sendSMS(new URL(smsURL), accessToken, smsRequest);
+			logger.info("SMS response is : " + response);
+			if (parseJson(response).get("generalErrors")== null) {
+				if (parseJson(response).get("fieldErrors")== null) {
+					logger.info("SMS sent successfully");									
+				} else {
+					logger.info("SMS was not sent because " + (parseJson(response).get("fieldErrors"))) ;	
+				}
+			} else {
+				logger.info("SMS was not sent because " + (parseJson(response).get("generalErrors"))) ;
+			};
+			System.out.println("SMS Reponse is " + response);	
+			response = null;
+			session.transfer(flowFile, success);
 		} catch (MalformedURLException  e) {
 			session.transfer(flowFile, failure);
 			logger.error("Error" + e.getMessage());
+		} catch (JSONException  e) {
+			session.transfer(flowFile, failure);
+			logger.error("Error : Invalid format of token return" + e.getMessage());
+		} catch (RuntimeException  e) {
+			session.transfer(flowFile, failure);
+			logger.error("Error" + e.getMessage());
 		} 
+	}
+
+	public String formRequestXMLBody(String request) {
+		String req;
+		req = "<Envelope><Body><SendMailing>\r\n" +
+				"<MailingId>1248923</MailingId>\r\n" +
+				"<RecipientEmail>trip.ankit87@gmail.com</RecipientEmail>\r\n" +
+				"</SendMailing>\r\n" +
+				"</Body></Envelope>";
+
+		return req;
+	}
+
+	public String formRequestJSONBody(String request) {
+		String req;
+		req = "{\"content\": \"Hey Ankit! You have suucessfully tested your NiFi processor.\"," +
+				" \"contacts\": [" +
+				" { " +                                               
+				"  \"contactLookup\": [ "+
+				"   { "+
+				"    \"name\": \"Mobile_No\", "+
+				"    \"value\": \"918095956000\", "+
+				"   \"channel\": \"SMS\" "+
+				"  } "+
+				"  ] "+
+				"  } "+
+				" ], "+
+				"  \"channelQualifier\": \"145024\" " +
+				"}" ;
+
+		return req;
+	}
+
+	/*
+	 * Read JSON response
+	 */
+
+	public JSONObject parseJson(String response)  {
+		System.out.println(response);
+		JSONObject json = (JSONObject) JSONSerializer.toJSON(response);
+		return json;        
+	}   
+	/*
+	 * Parse XML response to get Success Status
+	 */
+
+	public String getSuccessResult(String response) {
+		String result="result";
+		Pattern pattern = Pattern.compile(SUCCESS);
+		Matcher match = pattern.matcher(response);
+		if (match.find(1)) {
+			result= (match.group(1));
+		}
+		return result;
+	}
+
+	/*
+	 * Parse XML response to get fault code
+	 */
+	public String getFailResult(String response) {
+
+		String result="failed";
+		Pattern pattern = Pattern.compile(FAULT_CODE);
+
+		Matcher match = pattern.matcher(response);
+		if (match.find(1)) {
+			result= (match.group(1));
+		}
+		return result;	
+	}
+
+	/*
+	 * Parse XML response to get fault detail
+	 */
+	public String getFailDetail(String response) {
+
+		String detailReason="Reason";
+		Pattern pattern = Pattern.compile(FAULT_MESSAGE);
+		Matcher match = pattern.matcher(response);
+		if (match.find(1)) {
+			detailReason= (match.group(1));
+		}
+		return detailReason;	
 	}
 
 	/**
 	 * Set email properties
 	 */
 
-	public HttpsURLConnection sendEmail(URL emailURL) {
+	public String getTokenResponse(String clientID, String clientSecretID, String clientRefreshToken, String proxyHost, String proxyPort) {
 
-		HttpsURLConnection urlConnection;
+		String tokenResponse =null;
+		AccessTokenRetriever accessTokenRetriever = new AccessTokenRetriever(wcaAuthURL);
 		try {
-			if (isProxyset) {
-				urlConnection = (HttpsURLConnection)emailURL.openConnection(proxy);
+			if (!proxyHost.equals(SPACE)) {
+				tokenResponse = accessTokenRetriever.retrieveToken(clientID ,clientSecretID, clientRefreshToken, proxyHost, Integer.parseInt(proxyPort));
+				logger.info("Token with proxy" + tokenResponse);
+				proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, Integer.parseInt(proxyPort)));
+				isProxyset = true;
+			} else {		
+				logger.info("Token without proxy");
+				isProxyset = false;
+				tokenResponse = accessTokenRetriever.retrieveToken(clientID ,clientSecretID, clientRefreshToken);	
+				logger.info("Token without proxy" + tokenResponse);
 			}
-			else {
-				urlConnection = (HttpsURLConnection)emailURL.openConnection();
-			}
-			urlConnection.setRequestMethod("POST");
-			urlConnection.setDoOutput(true);
-			urlConnection.setRequestProperty("Content-Type","text/xml;charset=" + sendEncoding);
-			return urlConnection;
-		} catch (Exception e) {
+
+		} catch (MalformedURLException  e) {
 			logger.error("Error" + e.getMessage());
-			return null;
+		} catch (JSONException  e) {
+			logger.error("Error : Invalid format of token return" + e.getMessage());
+		} catch (IOException  e) {
+			logger.error("Error" + e.getMessage());
+		} catch (RuntimeException  e) {
+			logger.error("Error" + e.getMessage());
 		}
+
+		JSONObject readTokenResponse = (JSONObject) JSONSerializer.toJSON(tokenResponse);
+
+		if (readTokenResponse.containsKey("error")) {
+			logger.error(readTokenResponse.getString("error") + " Reason : " + readTokenResponse.getString("error_description"));
+			return readTokenResponse.getString("error");
+		}
+		return readTokenResponse.getString("access_token");
 	}
 
-	/**
-	 * Set SMS properties
-	 */
-	public HttpURLConnection sendSMS(URL smsURL) {
-
-		HttpURLConnection smsURLConn;
-		try {
-			if (isProxyset) {
-				smsURLConn = (HttpURLConnection)smsURL.openConnection(proxy);
-			}
-			else {
-				smsURLConn = (HttpURLConnection)smsURL.openConnection();
-			}
-			smsURLConn.setRequestMethod("POST");
-			smsURLConn.setDoOutput(true);
-			smsURLConn.setRequestProperty("Content-Type","application/json");
-			logger.info("##### In sendSMS");
-			return smsURLConn;
-		} catch (Exception e) {
-			logger.error("Error" + e.getMessage());
-			return null;
-		}
-	}
-
-	/**
-	 * Get https response of email
-	 */
-	public String getHttpsResponse( HttpsURLConnection httpsURLConnection, String accessToken, String request) {
+	public String sendEmail(URL emailURL, String accessToken, String request) {
 		OutputStream out;
 		InputStream in;
-		if (accessTokenSet) {
-			httpsURLConnection.setRequestProperty("Authorization", "Bearer " + accessToken );
-			accessTokenSet=false;
-		}
-
+		HttpsURLConnection httpsURLConnection;
 		try {
+			if (isProxyset) {
+				httpsURLConnection = (HttpsURLConnection)emailURL.openConnection(proxy);
+			}
+			else {
+				httpsURLConnection = (HttpsURLConnection)emailURL.openConnection();
+			}
+			httpsURLConnection.setRequestMethod("POST");
+			httpsURLConnection.setDoOutput(true);
+			httpsURLConnection.setRequestProperty("Content-Type","text/xml;charset=" + sendEncoding);
+			httpsURLConnection.setRequestProperty("Authorization", "Bearer " + accessToken );
 			httpsURLConnection.connect();
-			httpsURLConnection.getResponseCode();
 			out = httpsURLConnection.getOutputStream();
 			out.write(request.getBytes(sendEncoding));
 			out.flush();
-			System.out.println(httpsURLConnection.getResponseCode());
+			logger.info("Email Response code is " + httpsURLConnection.getResponseCode());
 			in = httpsURLConnection.getInputStream();
 			InputStreamReader inReader = new InputStreamReader(in, sendEncoding);
 			StringBuffer responseBuffer = new StringBuffer();
@@ -343,36 +463,35 @@ public class ConnectToUnica extends AbstractProcessor {
 			String response = responseBuffer.toString();
 			httpsURLConnection.disconnect();
 			return response;
-
-		} catch (MalformedURLException  e) {
-			logger.error("Error" + e.getMessage());
-			return null;
-		} catch (ProtocolException e) {
-			logger.error("Error" + e.getMessage());
-			return null;
-		} catch (IOException e) {
+		} catch (Exception e) {
 			logger.error("Error" + e.getMessage());
 			return null;
 		}
 	}
 
 	/**
-	 * Get http response of SMS
+	 * Set SMS properties
 	 */
-	public String getHttpResponse( HttpURLConnection httpURLConnection, String accessToken, String request) {
+	public String sendSMS(URL smsURLConn, String accessToken, String request) {
 		OutputStream out;
 		InputStream in;
-		if (accessTokenSet) {
-			httpURLConnection.setRequestProperty("Authorization", "Bearer " + accessToken );
-			accessTokenSet=false;
-		}
+		HttpURLConnection httpURLConnection;
 		try {
+			if (isProxyset) {
+				httpURLConnection = (HttpURLConnection)smsURLConn.openConnection(proxy);
+			}
+			else {
+				httpURLConnection = (HttpURLConnection)smsURLConn.openConnection();
+			}
+			httpURLConnection.setRequestMethod("POST");
+			httpURLConnection.setDoOutput(true);
+			httpURLConnection.setRequestProperty("Content-Type","application/json;charset=" + sendEncoding);
+			httpURLConnection.setRequestProperty("Authorization", "Bearer " + accessToken );
 			httpURLConnection.connect();
-			httpURLConnection.getResponseCode();
 			out = httpURLConnection.getOutputStream();
 			out.write(request.getBytes(sendEncoding));
 			out.flush();
-			System.out.println(httpURLConnection.getResponseCode());
+			logger.info("SMS Response code is " + httpURLConnection.getResponseCode());
 			in = httpURLConnection.getInputStream();
 			InputStreamReader inReader = new InputStreamReader(in, sendEncoding);
 			StringBuffer responseBuffer = new StringBuffer();
@@ -384,7 +503,6 @@ public class ConnectToUnica extends AbstractProcessor {
 			String response = responseBuffer.toString();
 			httpURLConnection.disconnect();
 			return response;
-
 		} catch (MalformedURLException  e) {
 			logger.error("Error" + e.getMessage());
 			return null;
@@ -396,4 +514,5 @@ public class ConnectToUnica extends AbstractProcessor {
 			return null;
 		}
 	}
+
 }
